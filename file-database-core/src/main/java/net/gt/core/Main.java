8e906e2f5470c575c0b45c492be7f42178460146
jason <xupengan@hanlin-tech.net>
2022-12-12 21:57:19 +0800

    Initial commit

---
+++ file-database-core/src/main/java/net/gt/core/Main.java
@@ -0,0 +1,13 @@
+package net.gt;
+
+/**
+ * 类描述
+ *
+ * @author gt-it
+ * @since 2022-12-12
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ file-database-spring-boot-starter/src/main/java/net/gt/Main.java
@@ -0,0 +1,13 @@
+package net.gt;
+
+/**
+ * 类描述
+ *
+ * @author gt-it
+ * @since ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
